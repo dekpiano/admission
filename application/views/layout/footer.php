f29bc3d9e5719ae8e787497e142b60c13d913ae6
dekpiano <dekpiano4815@gmail.com>
2022-03-08 22:34:18 +0700

    8/3/65 update

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -79,7 +79,7 @@
          <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.5/dist/sweetalert2.all.min.js"></script>
          <script src="<?=base_url()?>asset/js/ShowPerviewImg.js?v=2"></script>
 
-         <script src="<?=base_url()?>asset/js/CountdownTimer.js?v=2"></script>
+         <script src="<?=base_url()?>asset/js/CountdownTimer.js?v=3"></script>
 
 
 
